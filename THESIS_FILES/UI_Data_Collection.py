452a2bf78a7034aea1a4204633bac1a5ddc90994
Peter Justin Datingaling <peterjustindatingaling.main@gmail.com>
2024-08-29 00:50:06 +0800

    CONVERT H5

--- THESIS_FILES/UI_Data_Collection.py
+++ THESIS_FILES/UI_Data_Collection.py
@@ -25,14 +25,14 @@ def setWindowParameters(window):
     #Setting in center of the screen
     screenWidth = window.winfo_screenwidth()
     screenHeight = window.winfo_screenheight()
-    x = (screenWidth - window.winfo_reqwidth()) // 3.5
+    x = (screenWidth - window.winfo_reqwidth()) // 3.9
     y = (screenHeight - window.winfo_reqheight()) // 10
     
     #Lock the window dimensions
-    window.resizable(height=None, width=None)
+    window.resizable(False, False)
     
     #Set window size & window coordinates
-    window.geometry(f"700x600+{int(x)}+{int(y)}")
+    window.geometry(f"900x600+{int(x)}+{int(y)}")
 
 def detectHuman(frame, humanPoseDetectorModel, confidenceRate):
     
@@ -46,17 +46,6 @@ def detectHuman(frame, humanPoseDetectorModel, confidenceRate):
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
     return image, poseResults
-    
-
-def hide_button(widget): #REMOVE WIDGET (BUTTONS, LABELS)
-    widget.pack_forget()
-    
-def show_button(widget, widgetNumber): #RECOVER WIDGETS
-    if widgetNumber == 0: #OPEN CAMERA BUTTON
-        widget.place(relx=0.3, rely=0.8)
-
-    if widgetNumber==1:
-        widget.place(relx=0.6, rely=0.8)
         
 def execDetect():
     global isDetect
@@ -78,14 +67,17 @@ def drawUIelements(root):
     #QUIT BUTTON
     quitButton = Button(root, text="Quit", command=root.destroy).place(relx=0.93, rely=0.01)
     
-    #NUMBER 0
+    #OPEN CAMERA BUTTON
     openCameraButton = Button(root, text="Open Camera", command=showCamera)
-    openCameraButton.place(relx=0.3, rely=0.92)
+    openCameraButton.place(relx=0.1, rely=0.92)
     
-    #NUMBER 1
+    #DETECT BUTTON
     detectButton = Button(root, text="Start Detecting", command=execDetect)
-    detectButton.place(relx=0.6, rely=0.92)
+    detectButton.place(relx=0.2, rely=0.92)
     detectButton.config(state=DISABLED)
+    
+    folderName = Text(root, height=1, width=20)
+    folderName.place(relx=0.5, rely=0.92)
 
 
 def drawLandmarks(image, poseResults):
@@ -167,7 +159,7 @@ def main():
     #CAMERA PARAMETERS AREA
     args = parse_arguments()
     frameWidth, frameHeight = args.webcam_resolution
-    cameraInput = 0
+    cameraInput = 1
     camera = cv2.VideoCapture(cameraInput)  # Use the specified camera
     camera.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)
     camera.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)
